28722fabb15f192423d0e627ec372986e41092fd
Hasenpfote <hasenpfote36@gmail.com>
2016-07-03 00:59:33 +0900

    テストケースの追加と整理.

--- Logger/Logger/main.cpp
+++ Logger/Logger/main.cpp
@@ -1,44 +1,38 @@
-﻿#include <sstream>
+﻿#include <iostream>
+#include <sstream>
 #include <thread>
 #include "logger.h"
 #include "console_log.h"
 #include "file_log.h"
+#include "rolling_file_log.h"
 
-static Logger logger(std::make_shared<ConsoleLog>());
-//static Logger logger(std::make_shared<FileLog>("log.txt"));
+//static Logger logger(std::make_shared<ConsoleLog>());
+//static Logger logger(std::make_shared<FileLog>("Logs/log.txt"));
+static Logger logger(std::make_shared<RollingFileLog>("Logs/log.txt", 10));
 
-static constexpr auto max = 10;
+static constexpr auto max_output = 1000;
 
-void do_worker1()
+void do_worker()
 {
     auto i = 0;
+    auto id = std::this_thread::get_id();
     std::ostringstream oss;
-    oss << std::this_thread::get_id();
-    while(i < max){
+    while(i < max_output){
+        oss << "[" << id << "] - " << i;
         logger.Log(Logger::Severity::Debug, __FILE__, __LINE__, oss.str());
         i++;
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
-    }
-}
-
-void do_worker2()
-{
-    auto i = 0;
-    std::ostringstream oss;
-    oss << std::this_thread::get_id();
-    while(i < max){
-        logger.Log(Logger::Severity::Error, __FILE__, __LINE__, oss.str());
-        i++;
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        oss.str("");
+        oss.clear(std::stringstream::goodbit);
     }
 }
 
 void main()
 {
     //logger.SetSeverity(Logger::Severity::Fatal);
-
-    std::thread t1(do_worker1);
-    std::thread t2(do_worker2);
+    std::thread t1(do_worker);
+    std::thread t2(do_worker);
     t1.join();
     t2.join();
+    std::cout << "Done." << std::endl;
 }
