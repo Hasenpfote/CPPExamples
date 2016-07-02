7ed86c374849b9e67c5a0d9e23cc1dbb2085be38
Hasenpfote <hasenpfote36@gmail.com>
2016-07-03 00:59:07 +0900

    RollingLogFile クラスの追加.

---
+++ Logger/Logger/rolling_file_log.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include <cassert>
+#include <memory>
+#include <fstream>
+#include <filesystem>
+#include "log.h"
+
+class RollingFileLog final : public ILog
+{
+private:
+    std::unique_ptr<std::ofstream> ofs;
+    std::tr2::sys::path filepath;
+    std::size_t max_file_size;
+    int max_files;
+
+public:
+    RollingFileLog(const std::tr2::sys::path& filepath, int max_files = 1, std::size_t max_file_size = 1024)
+        : ofs(std::make_unique<std::ofstream>()), filepath(filepath), max_files(max_files), max_file_size(max_file_size)
+    {
+        assert(max_files > 0);
+    }
+    ~RollingFileLog() = default;
+    void Write(const std::string& buffer) override;
+};
