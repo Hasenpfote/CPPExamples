83c4811beec65432fb08a36cf922f91e17ddb8ab
Hasenpfote <hasenpfote36@gmail.com>
2016-07-03 15:10:17 +0900

    整理.

--- Logger/Logger/main.cpp
+++ Logger/Logger/main.cpp
@@ -5,8 +5,13 @@
 #include "file_log.h"
 #include "rolling_file_log.h"
 
+#define ENABLE_SINGLETON_LOGGER
 #define ENABLE_LOGGER2
-#ifndef ENABLE_LOGGER2
+
+#ifdef ENABLE_SINGLETON_LOGGER
+#include "singleton_logger.h"
+#else
+ #ifndef ENABLE_LOGGER2
 #include "logger.h"
 static Logger logger(std::make_shared<ConsoleLog>());
 //static Logger logger(std::make_shared<FileLog>("Logs/log.txt"));
@@ -14,6 +19,7 @@ static Logger logger(std::make_shared<ConsoleLog>());
 #else
 #include "logger2.h"
 static Logger2 logger;
+#endif
 #endif
 
 static constexpr auto max_output = 1000;
@@ -25,10 +31,14 @@ void do_worker()
     std::ostringstream oss;
     while(i < max_output){
         oss << "[" << id << "] - " << i;
-#ifndef ENABLE_LOGGER2
-        logger.Log(Logger::Severity::Debug, __FILE__, __LINE__, oss.str());
+#ifdef ENABLE_SINGLETON_LOGGER
+        SingletonLogger::GetInstance().Log(Logger2::Severity::Debug, __FILE__, __LINE__, oss.str());
 #else
+ #ifndef ENABLE_LOGGER2
+        logger.Log(Logger::Severity::Debug, __FILE__, __LINE__, oss.str());
+ #else
         logger.Log(Logger2::Severity::Debug, __FILE__, __LINE__, oss.str());
+ #endif
 #endif
         i++;
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
@@ -39,11 +49,16 @@ void do_worker()
 
 void main()
 {
-#ifdef ENABLE_LOGGER2
+#ifdef ENABLE_SINGLETON_LOGGER
+    SingletonLogger::GetInstance().SetSeverity(Logger2::Severity::Debug);
+    SingletonLogger::GetInstance().Provide(std::make_shared<ConsoleLog>());
+#else
+ #ifdef ENABLE_LOGGER2
     //logger.Provide(std::make_shared<RollingFileLog>("Logs/log.txt", 10));
     logger.Provide(std::make_shared<ConsoleLog>());
-#endif
+ #endif
     //logger.SetSeverity(Logger::Severity::Fatal);
+#endif
     std::thread t1(do_worker);
     std::thread t2(do_worker);
     t1.join();
