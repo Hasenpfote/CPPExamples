03b566d48c17ec510e76e2628eccfcd808f72db7
Hasenpfote <hasenpfote36@gmail.com>
2016-07-03 22:26:58 +0900

    no message

--- Logger2/Logger2/main.cpp
+++ Logger2/Logger2/main.cpp
@@ -5,28 +5,30 @@
 #include "console_appender.h"
 #include "rolling_file_appender.h"
 
-static constexpr auto max_output = 1000;
+static constexpr auto max_output = 10;
 
 static void do_worker()
 {
     auto i = 0;
     auto id = std::this_thread::get_id();
-    std::ostringstream oss;
     auto& inst = Logger::GetInstance();
     while(i < max_output){
-        oss << "[" << id << "] - " << i;
-        inst.Log(Logger::Severity::Debug, __FILE__, __LINE__, oss.str());
+        LOG_D("[" << id << "] - " << i);
         i++;
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        oss.str("");
-        oss.clear(std::stringstream::goodbit);
     }
 }
 
 void main()
 {
+#ifdef NDEBUG
+    Logger::GetInstance().SetSeverity(Logger::Severity::Fatal);
+#else
+    Logger::GetInstance().SetSeverity(Logger::Severity::Verbose);
+#endif
     Logger::GetInstance().AddAppender<mylog::ConsoleAppender>(std::make_shared<mylog::ConsoleAppender>());
     Logger::GetInstance().AddAppender<mylog::RollingFileAppender>(std::make_shared<mylog::RollingFileAppender>("Logs/log.txt", 10));
+    Logger::GetInstance().RemoveAppender<mylog::RollingFileAppender>();
 
     std::thread t1(do_worker);
     std::thread t2(do_worker);
