f92e3a346ab17e91d732b9b8029a1f69490c2be1
Hasenpfote <hasenpfote36@gmail.com>
2016-07-06 11:09:05 +0900

    スレッドテストコードの追加.

--- ServiceLocator2/ServiceLocator2/service_locator.h
+++ ServiceLocator2/ServiceLocator2/service_locator.h
@@ -10,6 +10,12 @@
 #include <cassert>
 #include <typeindex>
 
+#define ENABLE_THREAD_SAFE
+
+#ifdef ENABLE_THREAD_SAFE
+#include <mutex>
+#endif
+
 namespace example{ namespace service{
 
 class IService;
@@ -28,6 +34,9 @@ public:
     template<typename T>
     void RegisterService(const std::shared_ptr<IService>& service)
     {
+#ifdef ENABLE_THREAD_SAFE
+        std::lock_guard<std::mutex> lg(m);
+#endif
         static_assert(std::is_base_of<IService, T>::value == true, "T must be derived from IService.");
         this->service[typeid(T)] = service;
     }
@@ -35,6 +44,9 @@ public:
     template<typename T>
     void UnregisterService()
     {
+#ifdef ENABLE_THREAD_SAFE
+        std::lock_guard<std::mutex> lg(m);
+#endif
         static_assert(std::is_base_of<IService, T>::value == true, "T must be derived from IService.");
         auto& it = service.find(typeid(T));
         if(it != service.end()){
@@ -45,17 +57,23 @@ public:
     template<typename T>
     std::weak_ptr<T> GetService() const
     {
+#ifdef ENABLE_THREAD_SAFE
+        std::lock_guard<std::mutex> lg(m);
+#endif
         static_assert(std::is_base_of<IService, T>::value == true, "T must be derived from IService.");
         auto& it = service.find(typeid(T));
         if(it != service.cend()){
             return std::dynamic_pointer_cast<T>(it->second);
         }
-        assert(!"Unbale to find a service.");
+        //assert(!"Unbale to find a service.");
         return std::weak_ptr<T>();
     }
 
 private:
     std::unordered_map<std::type_index, std::shared_ptr<IService>> service;
+#ifdef ENABLE_THREAD_SAFE
+    mutable std::mutex m;
+#endif
 };
 
 }}
