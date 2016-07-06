54c3b7a72783d24d4475f34c3315db23db769dfa
Hasenpfote <hasenpfote36@gmail.com>
2016-07-06 11:08:31 +0900

    新規作成.

---
+++ ServiceLocator2/ServiceLocator2/audio_service.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include "service.h"
+
+class AudioService : public example::service::IService
+{
+public:
+    AudioService() = default;
+    virtual ~AudioService() = default;
+    virtual void Play(const std::string& buffer)
+    {
+        //std::cout << buffer << std::endl;
+    }
+};
