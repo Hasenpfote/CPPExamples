7173ba79dedaa7cc5d97067d9529629046f7b20c
Hasenpfote <hasenpfote36@gmail.com>
2016-07-09 06:31:49 +0900

    コンストラクタの実装の隠蔽.

--- Logger2/Logger2/file_appender.cpp
+++ Logger2/Logger2/file_appender.cpp
@@ -2,6 +2,11 @@
 
 namespace example{ namespace mylog{
 
+FileAppender::FileAppender(const std::tr2::sys::path& filepath)
+    : ofs(std::make_unique<std::ofstream>(filepath))
+{
+}
+
 void FileAppender::Write(const std::string& buffer)
 {
     *ofs << buffer.c_str() << std::endl;
