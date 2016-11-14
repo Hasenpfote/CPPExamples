65c70eaf28797dc6298f0f0a467ef611ea31fe0d
Hasenpfote <hasenpfote36@gmail.com>
2016-11-14 10:37:13 +0900

    ・typeを修正. ・宣言と定義の分離.

--- ExpressionTemplate/sources/vector2.h
+++ ExpressionTemplate/sources/vector2.h
@@ -9,7 +9,7 @@ class vector2 final
 {
     static constexpr auto N = 2;
 public:
-    using type = scalar<T>;
+    using type = vector2<T>;
     using value_type = T;
     using reference = value_type&;
     using const_reference = const value_type&;
@@ -18,37 +18,17 @@ public:
     vector2() = default;
     ~vector2() = default;
 
-    explicit vector2(const_reference other)
-    {
-        array = other.array;
-    }
+    explicit vector2(const_reference other);
 
     template<typename E>
-    explicit vector2(const E& expression)
-    {
-        for(auto i = 0; i < N; i++)
-            array[i] = expression[i];
-    }
+    explicit vector2(const E& expression);
 
-    vector2(value_type x, value_type y)
-    {
-        array[0] = x;
-        array[1] = y;
-    }
+    vector2(value_type x, value_type y);
 
-    reference operator = (const_reference other)
-    {
-        array = other.array;
-        return *this;
-    }
+    reference operator = (const_reference other);
 
     template<typename E>
-    vector2& operator = (const E& expression)
-    {
-        for(auto i = 0; i < N; i++)
-            array[i] = expression[i];
-        return *this;
-    }
+    vector2& operator = (const E& expression);
 
     reference operator [] (std::size_t i){ return array[i]; }
     value_type operator [] (std::size_t i) const { return array[i]; }
@@ -61,6 +41,43 @@ public:
 private:
     std::array<value_type, N> array;
 };
+
+template<typename T>
+vector2<T>::vector2(const_reference other)
+{
+    array = other.array;
+}
+
+template<typename T>
+template<typename E>
+vector2<T>::vector2(const E& expression)
+{
+    for(auto i = 0; i < N; i++)
+        array[i] = expression[i];
+}
+
+template<typename T>
+vector2<T>::vector2(value_type x, value_type y)
+{
+    array[0] = x;
+    array[1] = y;
+}
+
+template<typename T>
+typename vector2<T>::reference vector2<T>::operator = (const_reference other)
+{
+    array = other.array;
+    return *this;
+}
+
+template<typename T>
+template<typename E>
+vector2<T>& vector2<T>::operator = (const E& expression)
+{
+    for(auto i = 0; i < N; i++)
+        array[i] = expression[i];
+    return *this;
+}
 
 template<typename T>
 std::ostream& operator << (std::ostream& os, const vector2<T>& v)
