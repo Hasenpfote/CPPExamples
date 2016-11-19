2318a56aae1e6a9da7778f01b36ba099dc1d62fb
Hasenpfote <hasenpfote36@gmail.com>
2016-11-19 16:31:33 +0900

    新規作成.

---
+++ Math/sources/main.cpp
@@ -0,0 +1,171 @@
+﻿#include <iostream>
+#include <cassert>
+#include "matrix.h"
+
+using namespace example::math;
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+auto operator + (const matrix<T, M, N, Order>& lhs, const matrix<T, M, N, Order>& rhs)
+{
+    using type = matrix<T, M, N, Order>;
+    type result;
+    for(typename type::size_type i = 0; i < type::size(); i++)
+        result[i] = lhs[i] + rhs[i];
+    return result;
+}
+
+//column vector notation with column major order.
+template<typename T, std::size_t M, std::size_t N, std::size_t P,
+         typename = typename std::enable_if<!is_row_matrix<N, P>::value>::type>
+auto operator * (const matrix<T, M, N, column_major_order>& lhs, const matrix<T, N, P, column_major_order>& rhs)
+{
+    std::cout << "column_major_order" << std::endl;
+
+    using type = matrix<T, M, P, column_major_order>;
+    type result;
+    typename type::value_type elem;
+    for(typename type::size_type i = 0; i < P; i++){
+        for(typename type::size_type j = 0; j < M; j++){
+            elem = 0;
+            for(typename type::size_type k = 0; k < N; k++){
+                elem += lhs(j, k) * rhs(k, i);
+            }
+            result(j,i) = elem;
+        }
+    }
+    return result;
+}
+
+// row matrix notation and row major order.
+template<typename T, std::size_t M, std::size_t N, std::size_t P,
+         typename = typename std::enable_if<!is_column_matrix<N, P>::value>::type>
+auto operator * (const matrix<T, M, N, row_major_order>& lhs, const matrix<T, N, P, row_major_order>& rhs)
+{
+    std::cout << "row_major_order" << std::endl;
+
+    using type = matrix<T, M, P, row_major_order>;
+    type result;
+    typename type::value_type elem;
+    for(typename type::size_type i = 0; i < M; i++){
+        for(typename type::size_type j = 0; j < P; j++){
+            elem = 0;
+            for(typename type::size_type k = 0; k < N; k++){
+                elem += lhs(i, k) * rhs(k, j);
+            }
+            result(i, j) = elem;
+        }
+    }
+    return result;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order,
+         typename = typename std::enable_if<is_vector<M, N>::value>::type>
+auto dot_product(const matrix<T, M, N, Order>& lhs, const matrix<T, M, N, Order>& rhs)
+{
+    using type = matrix<T, M, N, Order>;
+    typename type::value_type result = 0;
+    for(typename type::size_type i = 0; i < type::size(); i++)
+        result += lhs[i] * rhs[i];
+    return result;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order,
+         typename = typename std::enable_if<is_n_dimensional_vector<M, N, 2>::value>::type>
+typename matrix<T, M, N, Order>::value_type cross_product(const matrix<T, M, N, Order>& lhs, const matrix<T, M, N, Order>& rhs)
+{
+    return lhs[0] * rhs[1] - lhs[1] * rhs[0];
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order,
+         typename = typename std::enable_if<is_n_dimensional_vector<M, N, 3>::value>::type>
+auto cross_product(const matrix<T, M, N, Order>& lhs, const matrix<T, M, N, Order>& rhs)
+{
+    using type = matrix<T, M, N, Order>;
+    type result;
+    result[0] = lhs[1] * rhs[2] - lhs[2] * rhs[1];
+    result[1] = lhs[2] * rhs[0] - lhs[0] * rhs[2];
+    result[2] = lhs[0] * rhs[1] - lhs[1] * rhs[0];
+    return result;
+}
+
+int main()
+{
+    {
+        constexpr auto dim = is_n_dimensional_vector<1, 3, 3>::value;
+    
+    }
+    {
+        std::cout << "=== column_major_order" << std::endl;
+        using matrix = matrix<float, 2, 3, column_major_order>;
+        matrix m({ 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f});
+        for(matrix::size_type i = 0; i < matrix::size(); i++){
+            auto row = matrix::row_subscript(i);
+            auto col = matrix::column_subscript(i);
+            std::cout << "index " << i << " => (" << row << ", " << col << ") = " << m(row, col) << std::endl;
+        }
+        std::cout << std::endl;
+    }
+    {
+        std::cout << "=== row_major_order" << std::endl;
+        using matrix = matrix<float, 2, 3, row_major_order>;
+        matrix m({ 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f });
+        for(matrix::size_type i = 0; i < matrix::size(); i++){
+            auto row = matrix::row_subscript(i);
+            auto col = matrix::column_subscript(i);
+            std::cout << "index " << i << " => (" << row << ", " << col << ") = " << m(row, col) << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    {
+        using matrix = matrix<float, 2, 2, column_major_order>;
+        //using matrix = matrix<float, 2, 2, row_major_order>;
+        matrix ma({ 1.0f, 2.0f, 3.0f, 4.0f });
+        matrix mb({ 1.0f, 2.0f, 3.0f, 4.0f });
+        matrix mc;
+
+        auto val11 = ma(0, 0);
+        auto val12 = ma(0, 1);
+        auto val21 = ma(1, 0);
+        auto val22 = ma(1, 1);
+
+        //mc = ma + mb;
+        //std::cout << mc << std::endl;
+        mc = ma * mb;
+        std::cout << mc << std::endl;
+    }
+    {
+        //using vector = matrix<float, 1, 2, row_major_order>;
+        //using matrix = matrix<float, 2, 2, row_major_order>;
+        using vector = matrix<float, 2, 1, column_major_order>;
+        using matrix = matrix<float, 2, 2, column_major_order>;
+
+        vector va({ 1.0f, 2.0f });
+        matrix m({ 1.0f, 2.0f, 3.0f, 4.0f });
+        vector v;
+        v = m * va;
+        //v = va * m;
+        std::cout << v << std::endl;
+    }
+    {
+        //using vector = matrix<float, 1, 2>;
+        using vector = matrix<float, 2, 1>;
+        vector a({ 1.0f, 2.0f });
+        vector b({ 2.0f, 3.0f });
+        auto dot = dot_product(a, b);
+        std::cout << dot << std::endl;
+    }
+    {
+        using vector = matrix<float, 1, 2>;
+        vector a({ 1.0f, 2.0f });
+        vector b({ 4.0f, 5.0f });
+        auto cross = cross_product(a, b);
+    }
+    {
+        using vector = matrix<float, 3, 1>;
+        vector a({ 1.0f, 2.0f, 3.0f });
+        vector b({ 4.0f, 5.0f, 6.0f });
+        auto cross = cross_product(a, b);
+    }
+    return 0;
+}
---
+++ Math/sources/matrix.h
@@ -0,0 +1,195 @@
+﻿#pragma once
+#include <array>
+#include <iostream>
+
+namespace example{ namespace math{
+
+// storage_order_traits
+struct storage_order_tag {};
+struct column_major_order_tag : storage_order_tag {};
+struct row_major_order_tag : storage_order_tag {};
+
+struct column_major_order {};
+struct row_major_order {};
+
+template<typename T>
+struct storage_order_traits
+{
+    using storage_order_category = column_major_order_tag;
+};
+
+template<>
+struct storage_order_traits<row_major_order>
+{
+    using storage_order_category = row_major_order_tag;
+};
+
+// is_square_matrix
+template<std::size_t M, std::size_t N, typename Enable = void>
+struct is_square_matrix
+    : std::false_type
+{
+};
+
+template<std::size_t M, std::size_t N>
+struct is_square_matrix<M, N, typename std::enable_if<M == N>::type>
+    : std::true_type
+{
+};
+
+// is_column_matrix
+template<std::size_t M, std::size_t N, typename Enable = void>
+struct is_column_matrix
+    : std::false_type
+{
+};
+
+template<std::size_t M, std::size_t N>
+struct is_column_matrix<M, N, typename std::enable_if<N == 1>::type>
+    : std::true_type
+{
+};
+
+// is_row_matrix
+template<std::size_t M, std::size_t N, typename Enable = void>
+struct is_row_matrix
+    : std::false_type
+{
+};
+
+template<std::size_t M, std::size_t N>
+struct is_row_matrix<M, N, typename std::enable_if<M == 1>::type>
+    : std::true_type
+{
+};
+
+// is_vector
+template<std::size_t M, std::size_t N, typename Enable = void>
+struct is_vector
+    : std::false_type
+{
+};
+
+template<std::size_t M, std::size_t N>
+struct is_vector<M, N,
+    typename std::enable_if<is_column_matrix<M, N>::value || is_row_matrix<M, N>::value>::type>
+    : std::true_type
+{
+};
+
+// is_n_dimensional_vector
+template<std::size_t M, std::size_t N, std::size_t D, typename Enable = void>
+struct is_n_dimensional_vector
+    : std::false_type
+{
+};
+
+template<std::size_t M, std::size_t N, std::size_t D>
+struct is_n_dimensional_vector<M, N, D,
+    typename std::enable_if<(is_column_matrix<M, N>::value && (M == D))
+    || (is_row_matrix<M, N>::value && (N == D))>::type>
+    : std::true_type
+{
+};
+
+// is_scalar
+template<std::size_t M, std::size_t N, typename Enable = void>
+struct is_scalar
+    : std::false_type
+{
+};
+
+template<std::size_t M, std::size_t N>
+struct is_scalar<M, N, typename std::enable_if<(M == 1) && (N == 1)>::type>
+    : std::true_type
+{
+};
+
+// matrix
+template<typename T, std::size_t M, std::size_t N = M, typename Order = column_major_order>
+class matrix final
+{
+public:
+    using type = matrix<T, M, N, Order>;
+    using value_type = T;
+    using size_type = std::size_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using storage_type = std::array<value_type, M * N>;
+
+public:
+    // Ctor and Dtor.
+    matrix() = default;
+    ~matrix() = default;
+
+    matrix(const type& other){ storage_ = other.storage_; }
+    explicit matrix(const storage_type& storage){ storage_ = storage; }
+
+    // Assignment operator.
+    type& operator = (const type& other){ storage_ = other.storage_; return *this; }
+
+    // Casting operator.
+    explicit operator pointer (){ return storage_.data(); }
+    explicit operator const_pointer () const { return storage_.data(); }
+
+    // Subscript operator.
+    reference operator [] (size_type i){ return storage_[i]; }
+    value_type operator [] (size_type i) const { return storage_[i]; }
+
+    reference operator () (size_type row, size_type column){ return storage_[index(row, column)]; }
+    value_type operator () (size_type row, size_type column) const { return storage_[index(row, column)]; }
+
+    //
+    static constexpr size_type rows(){ return M; }
+    static constexpr size_type columns(){ return N; }
+    static constexpr size_type size(){ return rows() * columns(); }
+
+    static constexpr size_type index(size_type row, size_type column)
+    {
+        return index(row, column, typename storage_order_traits<Order>::storage_order_category());
+    }
+
+    static constexpr size_type row_subscript(size_type index)
+    {
+        return row_subscript(index, typename storage_order_traits<Order>::storage_order_category());
+    }
+
+    static constexpr size_type column_subscript(size_type index)
+    {
+        return column_subscript(index, typename storage_order_traits<Order>::storage_order_category());
+    }
+
+private:
+    static constexpr size_type index(size_type row, size_type column, column_major_order_tag){ return column * rows() + row; }
+    static constexpr size_type index(size_type row, size_type column, row_major_order_tag){ return row * columns() + column; }
+
+    static constexpr size_type row_subscript(size_type index, column_major_order_tag){ return index % rows(); }
+    static constexpr size_type column_subscript(size_type index, column_major_order_tag){ return index / rows(); }
+
+    static constexpr size_type row_subscript(size_type index, row_major_order_tag){ return index / columns(); }
+    static constexpr size_type column_subscript(size_type index, row_major_order_tag){ return index % columns(); }
+
+private:
+    storage_type storage_;
+};
+
+// for debug
+template<typename T, std::size_t M, std::size_t N, typename Order>
+std::ostream& operator << (std::ostream& os, const matrix<T, M, N, Order>& m)
+{
+    using type = matrix<T, M, N, Order>;
+    const auto flags = os.flags();
+    for(typename type::size_type i = 0; i < type::rows(); i++){
+        os << "matrix(" << i << "){" << m(i, 0);
+        for(typename type::size_type j = 1; j < type::columns(); j++){
+            os << ", " << m(i, j);
+        }
+        os << "}" << std::endl;
+    }
+    os.flags(flags);
+    return os;
+}
+
+}}
