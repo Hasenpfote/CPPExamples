15634775146c6079b1efe093ebc102623da0a8a7
Hasenpfote <hasenpfote36@gmail.com>
2016-11-21 12:50:29 +0900

    定義の分離.

---
+++ Math/sources/impl/matrix_impl.h
@@ -0,0 +1,150 @@
+﻿#pragma once
+
+namespace example{ namespace math{
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+matrix<T, M, N, Order>::matrix(const type& other)
+{
+    storage_ = other.storage_;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+matrix<T, M, N, Order>::matrix(const storage_type& storage)
+{
+    storage_ = storage;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+typename matrix<T, M, N, Order>::type& matrix<T, M, N, Order>::operator=(const type & other)
+{
+    storage_ = other.storage_;
+    return *this;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+matrix<T, M, N, Order>::operator pointer ()
+{
+    return storage_.data();
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+matrix<T, M, N, Order>::operator const_pointer () const
+{
+    return storage_.data();
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+typename matrix<T, M, N, Order>::reference matrix<T, M, N, Order>::operator [] (size_type i)
+{
+    return storage_[i];
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+typename matrix<T, M, N, Order>::value_type matrix<T, M, N, Order>::operator [] (size_type i) const
+{
+    return storage_[i];
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+typename matrix<T, M, N, Order>::reference matrix<T, M, N, Order>::operator () (size_type row, size_type column)
+{
+    return storage_[index(row, column)];
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+typename matrix<T, M, N, Order>::value_type matrix<T, M, N, Order>::operator () (size_type row, size_type column) const
+{
+    return storage_[index(row, column)];
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::rows()
+{
+    return M;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::columns()
+{
+    return N;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::size()
+{
+    return rows() * columns();
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::index(size_type row, size_type column)
+{
+    return index(row, column, typename detail::storage_order_traits<Order>::storage_order_category());
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::row_subscript(size_type index)
+{
+    return row_subscript(index, typename detail::storage_order_traits<Order>::storage_order_category());
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::column_subscript(size_type index)
+{
+    return column_subscript(index, typename detail::storage_order_traits<Order>::storage_order_category());
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::index(size_type row, size_type column, detail::column_major_order_tag)
+{
+    return column * rows() + row;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::index(size_type row, size_type column, detail::row_major_order_tag)
+{
+    return row * columns() + column;
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::row_subscript(size_type index, detail::column_major_order_tag)
+{
+    return index % rows();
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::column_subscript(size_type index, detail::column_major_order_tag)
+{
+    return index / rows();
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::row_subscript(size_type index, detail::row_major_order_tag)
+{
+    return index / columns();
+}
+
+template<typename T, std::size_t M, std::size_t N, typename Order>
+constexpr typename matrix<T, M, N, Order>::size_type matrix<T, M, N, Order>::column_subscript(size_type index, detail::row_major_order_tag)
+{
+    return index % columns();
+}
+
+
+// for debug
+template<typename T, std::size_t M, std::size_t N, typename Order>
+std::ostream& operator << (std::ostream& os, const matrix<T, M, N, Order>& m)
+{
+    using type = matrix<T, M, N, Order>;
+    const auto flags = os.flags();
+    for(typename type::size_type i = 0; i < type::rows(); i++){
+        os << "matrix(" << i << "){" << m(i, 0);
+        for(typename type::size_type j = 1; j < type::columns(); j++){
+            os << ", " << m(i, j);
+        }
+        os << "}" << std::endl;
+    }
+    os.flags(flags);
+    return os;
+}
+
+}}
--- Math/sources/matrix.h
+++ Math/sources/matrix.h
@@ -5,24 +5,28 @@
 namespace example{ namespace math{
 
 // storage_order_traits
+struct column_major_order {};
+struct row_major_order {};
+
+namespace detail{
+
 struct storage_order_tag {};
 struct column_major_order_tag : storage_order_tag {};
 struct row_major_order_tag : storage_order_tag {};
 
-struct column_major_order {};
-struct row_major_order {};
-
 template<typename T>
 struct storage_order_traits
 {
-    using storage_order_category = column_major_order_tag;
+    using storage_order_category = detail::column_major_order_tag;
 };
 
 template<>
 struct storage_order_traits<row_major_order>
 {
-    using storage_order_category = row_major_order_tag;
+    using storage_order_category = detail::row_major_order_tag;
 };
+
+}
 
 // is_square_matrix
 template<std::size_t M, std::size_t N, typename Enable = void>
@@ -86,8 +90,8 @@ struct is_n_dimensional_vector
 
 template<std::size_t M, std::size_t N, std::size_t D>
 struct is_n_dimensional_vector<M, N, D,
-    typename std::enable_if<(is_column_matrix<M, N>::value && (M == D))
-    || (is_row_matrix<M, N>::value && (N == D))>::type>
+                               typename std::enable_if<(is_column_matrix<M, N>::value && (M == D))
+                                                    || (is_row_matrix<M, N>::value && (N == D))>::type>
     : std::true_type
 {
 };
@@ -124,72 +128,46 @@ public:
     matrix() = default;
     ~matrix() = default;
 
-    matrix(const type& other){ storage_ = other.storage_; }
-    explicit matrix(const storage_type& storage){ storage_ = storage; }
+    matrix(const type& other);
+    explicit matrix(const storage_type& storage);
 
     // Assignment operator.
-    type& operator = (const type& other){ storage_ = other.storage_; return *this; }
+    type& operator = (const type& other);
 
     // Casting operator.
-    explicit operator pointer (){ return storage_.data(); }
-    explicit operator const_pointer () const { return storage_.data(); }
+    explicit operator pointer ();
+    explicit operator const_pointer () const;
 
     // Subscript operator.
-    reference operator [] (size_type i){ return storage_[i]; }
-    value_type operator [] (size_type i) const { return storage_[i]; }
+    reference operator [] (size_type i);
+    value_type operator [] (size_type i) const;
 
-    reference operator () (size_type row, size_type column){ return storage_[index(row, column)]; }
-    value_type operator () (size_type row, size_type column) const { return storage_[index(row, column)]; }
+    reference operator () (size_type row, size_type column);
+    value_type operator () (size_type row, size_type column) const;
 
     //
-    static constexpr size_type rows(){ return M; }
-    static constexpr size_type columns(){ return N; }
-    static constexpr size_type size(){ return rows() * columns(); }
+    static constexpr size_type rows();
+    static constexpr size_type columns();
+    static constexpr size_type size();
 
-    static constexpr size_type index(size_type row, size_type column)
-    {
-        return index(row, column, typename storage_order_traits<Order>::storage_order_category());
-    }
-
-    static constexpr size_type row_subscript(size_type index)
-    {
-        return row_subscript(index, typename storage_order_traits<Order>::storage_order_category());
-    }
-
-    static constexpr size_type column_subscript(size_type index)
-    {
-        return column_subscript(index, typename storage_order_traits<Order>::storage_order_category());
-    }
+    static constexpr size_type index(size_type row, size_type column);
+    static constexpr size_type row_subscript(size_type index);
+    static constexpr size_type column_subscript(size_type index);
 
 private:
-    static constexpr size_type index(size_type row, size_type column, column_major_order_tag){ return column * rows() + row; }
-    static constexpr size_type index(size_type row, size_type column, row_major_order_tag){ return row * columns() + column; }
+    static constexpr size_type index(size_type row, size_type column, detail::column_major_order_tag);
+    static constexpr size_type index(size_type row, size_type column, detail::row_major_order_tag);
 
-    static constexpr size_type row_subscript(size_type index, column_major_order_tag){ return index % rows(); }
-    static constexpr size_type column_subscript(size_type index, column_major_order_tag){ return index / rows(); }
+    static constexpr size_type row_subscript(size_type index, detail::column_major_order_tag);
+    static constexpr size_type column_subscript(size_type index, detail::column_major_order_tag);
 
-    static constexpr size_type row_subscript(size_type index, row_major_order_tag){ return index / columns(); }
-    static constexpr size_type column_subscript(size_type index, row_major_order_tag){ return index % columns(); }
+    static constexpr size_type row_subscript(size_type index, detail::row_major_order_tag);
+    static constexpr size_type column_subscript(size_type index, detail::row_major_order_tag);
 
 private:
     storage_type storage_;
 };
 
-// for debug
-template<typename T, std::size_t M, std::size_t N, typename Order>
-std::ostream& operator << (std::ostream& os, const matrix<T, M, N, Order>& m)
-{
-    using type = matrix<T, M, N, Order>;
-    const auto flags = os.flags();
-    for(typename type::size_type i = 0; i < type::rows(); i++){
-        os << "matrix(" << i << "){" << m(i, 0);
-        for(typename type::size_type j = 1; j < type::columns(); j++){
-            os << ", " << m(i, j);
-        }
-        os << "}" << std::endl;
-    }
-    os.flags(flags);
-    return os;
-}
-
 }}
+
+#include "impl/matrix_impl.h"
