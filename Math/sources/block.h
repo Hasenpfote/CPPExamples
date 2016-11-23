3b54c8e69e2f11c6b2de36fc7112e8b4eb46b4f5
Hasenpfote <hasenpfote36@gmail.com>
2016-11-23 22:57:17 +0900

    Const correctness を考慮するよう修正.

--- Math/sources/block.h
+++ Math/sources/block.h
@@ -5,25 +5,45 @@ namespace example{ namespace math{
 template<typename T, std::size_t M, std::size_t N, typename Order>
 class Matrix;
 
-template<typename T, std::size_t M, std::size_t N, std::size_t P, std::size_t Q, typename Order>
+template<typename MatrixRefType, std::size_t Rows, std::size_t Columns>
 class Block final
 {
-    using size_type = std::size_t;
+    using matrix_type = typename std::remove_reference<MatrixRefType>::type;
+    using matrix_value_type = typename matrix_type::value_type;
+    using matrix_size_type = typename matrix_type::size_type;
+    using matrix_storage_order_type = typename matrix_type::storage_order_type;
+    using cast_type = Matrix<matrix_value_type, Rows, Columns, matrix_storage_order_type>;
 public:
-    Block(Matrix<T, M, N, Order>& ref, size_type row, size_type column)
+    Block(MatrixRefType ref, matrix_size_type row, matrix_size_type column)
         : ref_(ref), row_(row), column_(column)
-    {}
-
-    void operator = (const Matrix<T, P, Q, Order>& other)
     {
-        for (size_type i = 0; i < P; i++)
-            for (size_type j = 0; j < Q; j++)
-                ref_(row_ + i, column_ + j) = other(i, j);
     }
+
+    void operator = (const Matrix<matrix_value_type, Rows, Columns, matrix_storage_order_type>& other);
+    operator cast_type () const;
+
 private:
-    Matrix<T, M, N, Order>& ref_;
-    size_type row_;
-    size_type column_;
+    MatrixRefType ref_;
+    matrix_size_type row_;
+    matrix_size_type column_;
 };
+
+template<typename MatrixRefType, std::size_t Rows, std::size_t Columns>
+void Block<MatrixRefType, Rows, Columns>::operator = (const Matrix<matrix_value_type, Rows, Columns, matrix_storage_order_type>& other)
+{
+    for(matrix_size_type i = 0; i < Rows; i++)
+        for(matrix_size_type j = 0; j < Columns; j++)
+            ref_(row_ + i, column_ + j) = other(i, j);
+}
+
+template<typename MatrixRefType, std::size_t Rows, std::size_t Columns>
+Block<MatrixRefType, Rows, Columns>::operator cast_type () const
+{
+    cast_type result;
+    for(matrix_size_type i = 0; i < Rows; i++)
+        for(matrix_size_type j = 0; j < Columns; j++)
+            result(i, j) = ref_(row_ + i, column_ + j);
+    return result;
+}
 
 }}
