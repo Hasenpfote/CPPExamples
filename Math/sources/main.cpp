5ed1c8f83c65324ef094d723d35b54dc0e61fd46
Hasenpfote <hasenpfote36@gmail.com>
2016-11-23 22:58:48 +0900

    テストケースの更新.

--- Math/sources/main.cpp
+++ Math/sources/main.cpp
@@ -1,11 +1,76 @@
 ﻿#include <iostream>
 #include <cassert>
+#include <typeinfo>
 #include "matrix.h"
 
 using namespace example::math;
 
+
+template<typename T1, typename T2>
+struct traits
+{
+    typedef typename std::conditional<std::is_const<T1>::value,
+        typename std::add_const<T2>::type,
+        typename std::remove_const<T2>::type>::type type;
+};
+
+template<typename BarRef>
+struct foo
+{
+    foo() = default;
+    explicit foo(BarRef value)
+        : ref(value)
+    {
+        using type = typename std::remove_reference<BarRef>::type;
+        constexpr auto b = std::is_const<type>::value;
+        std::cout << b << std::endl;
+    }
+    BarRef ref;
+};
+
+template<typename T>
+struct bar
+{
+    bar()
+        : value_(0)
+    {}
+
+    auto func()
+    {
+        std::cout << "void func() ===" << std::endl;
+        using type = decltype(*this);
+        using type2 = typename std::remove_reference<type>::type;
+        constexpr auto value = std::is_const<type2>::value;
+        value_ = 1;
+        auto result = foo<type>(*this);
+        using foo_type = decltype(foo<type>::ref);
+        std::cout << "void func() ===" << std::endl;
+        return result;
+    }
+
+    auto func() const
+    {
+        std::cout << "void func() const ===" << std::endl;
+        using type = decltype(*this);
+        using type2 = typename std::remove_reference<type>::type;
+        constexpr auto value = std::is_const<type2>::value;
+        auto result = foo<type>(*this);
+        using foo_type = decltype(foo<type>::ref);
+        std::cout << "void func() const ===" << std::endl;
+        return result;
+    }
+    T value_;
+};
+
 int main()
 {
+    {
+        using bar_type = bar<int>;
+        bar_type b1;
+        const bar_type b2;
+        auto ret1 = b1.func();
+        auto ret2 = b2.func();
+    }
     {
         constexpr auto dim = is_n_dimensional_vector<1, 3, 3>::value;
     }
@@ -123,13 +188,16 @@ int main()
 
         Matrix2 m;
         m = m3a.block<2, 2>(1, 1);
-        m3a.block<2, 2>(1, 1) = m2a;
+        std::cout << "m3a" << std::endl;
         std::cout << m3a << std::endl;
-        //m3a.block(1, 1, m2a);
+        std::cout << "m3a.block<2, 2>(1, 1)" << std::endl;
+        std::cout << m << std::endl;
 
-        int a;
-        a = 1;
-    
+        m3a.block<2, 2>(1, 1) = m2a;
+        std::cout << "m2a" << std::endl;
+        std::cout << m2a << std::endl;
+        std::cout << "m3a.block<2, 2>(1, 1) = m2a" << std::endl;
+        std::cout << m3a << std::endl;
     }
 #endif
     return 0;
