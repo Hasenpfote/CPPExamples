0856a07654c7cd93cc6ceef27bf14a60244cbde7
Hasenpfote <hasenpfote36@gmail.com>
2016-12-08 16:56:35 +0900

    新規作成.

---
+++ ComparingFloatingPointNumbers/sources/fp.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+#include <type_traits>
+#include <limits>
+#include <cmath>
+#include <algorithm>
+
+/*!
+ * @param [in] a
+ * @param [in] b
+ * @param [in] abs_tolerance    Absolute tolerance -- the value should be less than relative tolerance.
+ * @param [in] rel_tolerance    Relative tolerance -- the value should be greater than or equal to epsilon.
+ * @return Which returns true if 'a' is approximately equal to 'b' or false otherwise.
+ * @note 
+ */
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+bool IsApproximatelyEqualTo(T a, T b, T abs_tolerance, T rel_tolerance = std::numeric_limits<T>::epsilon())
+{
+    // |a - b| <= max(abs_tolerance, max(|a|, |b|) * rel_tolerance)
+    return std::abs(a - b) <= std::max(abs_tolerance, std::max(std::abs(a), std::abs(b)) * rel_tolerance);
+}
+
+/*!
+ * @param [in] a
+ * @param [in] b
+ * @param [in] abs_tolerance    Absolute tolerance -- the value should be less than relative tolerance.
+ * @param [in] rel_tolerance    Relative tolerance -- the value should be greater than or equal to epsilon.
+ * @return Which returns true if 'a' is definitely greater than 'b' or false otherwise.
+ */
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+bool IsDefinitelyGreaterThan(T a, T b, T abs_tolerance, T rel_tolerance = std::numeric_limits<T>::epsilon())
+{
+    const auto diff = a - b;
+    // Check if the numbers are really close -- needed when comparing numbers near zero.
+    if(diff <= abs_tolerance)
+        return false;
+    return diff > (std::max(std::abs(a), std::abs(b)) * rel_tolerance);
+}
+
+/*!
+ * @param [in] a
+ * @param [in] b
+ * @param [in] abs_tolerance    Absolute tolerance -- the value should be less than relative tolerance.
+ * @param [in] rel_tolerance    Relative tolerance -- the value should be greater than or equal to epsilon.
+ * @return Which returns true if 'a' is definitely less than 'b' or false otherwise.
+ */
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+bool IsDefinitelyLessThan(T a, T b, T abs_tolerance, T rel_tolerance = std::numeric_limits<T>::epsilon())
+{
+    const auto diff = b - a;
+    // Check if the numbers are really close -- needed when comparing numbers near zero.
+    if(diff <= abs_tolerance)
+        return false;
+    return diff > (std::max(std::abs(a), std::abs(b)) * rel_tolerance);
+}
+
+/*!
+ * @param [in] a
+ * @param [in] b
+ * @param [in] abs_tolerance    Absolute tolerance -- the value should be less than relative tolerance.
+ * @param [in] rel_tolerance    Relative tolerance -- the value should be greater than or equal to epsilon.
+ * @return Which returns true if 'a' is greater than or approximately equal to 'b' or false otherwise.
+ */
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+bool IsGreaterThanOrApproximatelyEqualTo(T a, T b, T abs_tolerance, T rel_tolerance = std::numeric_limits<T>::epsilon())
+{
+    return IsDefinitelyGreaterThan(a, b, abs_tolerance, rel_tolerance) || IsApproximatelyEqualTo(a, b, abs_tolerance, rel_tolerance);
+}
+
+/*!
+ * @param [in] a
+ * @param [in] b
+ * @param [in] abs_tolerance    Absolute tolerance -- the value should be less than relative tolerance.
+ * @param [in] rel_tolerance    Relative tolerance -- the value should be greater than or equal to epsilon.
+ * @return Which returns true if 'a' is less than or approximately equal to 'b' or false otherwise.
+ */
+template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+bool IsLessThanOrApproximatelyEqualTo(T a, T b, T abs_tolerance, T rel_tolerance = std::numeric_limits<T>::epsilon())
+{
+    return IsDefinitelyLessThan(a, b, abs_tolerance, rel_tolerance) || IsApproximatelyEqualTo(a, b, abs_tolerance, rel_tolerance);
+}
---
+++ ComparingFloatingPointNumbers/sources/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <bitset>
+#include <cassert>
+#include "fp.h"
+
+template<typename T>
+void compare(T a, T b, T abs_tol, T rel_tol)
+{
+    auto eq = IsApproximatelyEqualTo(a, b, abs_tol, rel_tol);
+    auto gt = IsDefinitelyGreaterThan(a, b, abs_tol, rel_tol);
+    auto gte = IsGreaterThanOrApproximatelyEqualTo(a, b, abs_tol, rel_tol);
+    auto lt = IsDefinitelyLessThan(a, b, abs_tol, rel_tol);
+    auto lte = IsLessThanOrApproximatelyEqualTo(a, b, abs_tol, rel_tol);
+
+    std::cout << std::scientific << "a = " << a << std::endl;
+    std::cout << std::scientific << "b = " << b << std::endl;
+    std::cout << std::boolalpha << "a == b: " << eq << std::endl;
+    std::cout << std::boolalpha << "a >  b: " << gt << std::endl;
+    std::cout << std::boolalpha << "a >= b: " << gte << std::endl;
+    std::cout << std::boolalpha << "a <  b: " << lt << std::endl;
+    std::cout << std::boolalpha << "a <= b: " << lte << std::endl;
+    std::cout << std::endl;
+
+    assert((eq) ? (!gt && gte && !lt && lte) : true);
+    assert((!eq && gt) ? (gte && !lt && !lte) : true);
+    assert((!eq && lt) ? (!gt && !gte && lte) : true);
+}
+
+int main()
+{
+    const float abs_tol = 1e-10f;
+    const float rel_tol = std::numeric_limits<float>::epsilon();
+    float a, b;
+
+    a = -1.000000f;
+    b = -1.000001f;
+    compare(a, b, abs_tol, rel_tol);
+    a =  0.000000f;
+    b = -0.000001f;
+    compare(a, b, abs_tol, rel_tol);
+    a =  1.000000f;
+    b =  0.999999f;
+    compare(a, b, abs_tol, rel_tol);
+    a = 10.000000f;
+    b =  9.999999f;
+    compare(a, b, abs_tol, rel_tol);
+    a = 100.00000f;
+    b =  99.99999f;
+    compare(a, b, abs_tol, rel_tol);
+    a = 1000.0000f;
+    b =  999.9999f;
+    compare(a, b, abs_tol, rel_tol);
+    a = 10000.000f;
+    b =  9999.999f;
+    compare(a, b, abs_tol, rel_tol);
+    a = 100000.00f;
+    b =  99999.99f;
+    compare(a, b, abs_tol, rel_tol);
+
+    return 0;
+}
