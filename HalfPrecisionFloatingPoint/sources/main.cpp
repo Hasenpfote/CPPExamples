2ee73f9dd2f4bf5f7ba540fcbed9a7b887700231
Hasenpfote <hasenpfote36@gmail.com>
2016-12-20 10:33:07 +0900

    新規作成.

---
+++ HalfPrecisionFloatingPoint/sources/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include "fp_conversion.h"
+
+int main()
+{
+    const float s = 1.234000f;
+    auto h1 = ConvertSingleToHalf(s);
+    auto s1 = ConvertHalfToSingle(h1);
+
+    std::cout << s << std::endl;
+    std::cout << h1 << std::endl;
+    std::cout << s1 << std::endl;
+
+    return 0;
+}
