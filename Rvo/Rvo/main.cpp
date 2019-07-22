f81513ecb057ceb1c7d3b2fce879bd21a11719ea
Hasenpfote <Hasenpfote@gmail.com>
2019-07-23 08:43:05 +0900

    Update

---
+++ Rvo/Rvo/main.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+#include <vector>
+
+class Foo
+{
+public:
+    Foo()
+        : value()
+    {
+        std::cout << "Foo()" << std::endl;
+    }
+#if 0
+    Foo(const Foo&) = default;
+    Foo(Foo&& foo) = default;
+#else
+    //
+    Foo(const Foo& foo)
+    {
+        std::cout << "Foo(const Foo&)" << std::endl;
+        value = foo.value;
+    }
+    //
+    Foo(Foo&& foo)
+    {
+        std::cout << "Foo(Foo&&)" << std::endl;
+        value = foo.value;
+    }
+#endif
+    //
+    ~Foo()
+    {
+        std::cout << "~Foo()" << std::endl;
+    }
+    explicit Foo(int value)
+        : value(value)
+    {
+        std::cout << "Foo(int)" << std::endl;
+    }
+    int value;
+};
+
+static Foo gfoo1 = Foo(50);
+static Foo gfoo2 = Foo(60);
+
+Foo rvo1()
+{
+    return Foo(3);
+}
+
+Foo rvo2(bool cond)
+{
+    return (cond)? Foo(3) : Foo(4);
+}
+
+Foo rvo3(bool cond)
+{
+    return Foo((cond)? gfoo1 : gfoo2);
+}
+
+int main()
+{
+    std::cout << "--- start." << std::endl;
+    {
+        std::cout << "--- rvo1" << std::endl;
+        auto foo = rvo1();
+        (void)foo;
+        std::cout << "--- scope end." << std::endl;
+    }
+    std::cout << std::endl;
+    {
+        std::cout << "--- rvo2" << std::endl;
+        auto foo1 = rvo2(true);
+        (void)foo1;
+        auto foo2 = rvo2(false);
+        (void)foo2;
+        std::cout << "--- scope end." << std::endl;
+    }
+    std::cout << std::endl;
+    {
+        std::cout << "--- rvo3" << std::endl;
+        auto foo1 = rvo3(true);
+        std::cout << "--- " << foo1.value << std::endl;
+        auto foo2 = rvo3(false);
+        std::cout << "--- " << foo2.value << std::endl;
+        std::cout << "--- scope end." << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "--- done." << std::endl;
+    return 0;
+}
---
+++ SpecificInterface/SpecificInterface/main.cpp
@@ -0,0 +1,102 @@
+﻿#include "iostream"
+
+class SpecificInterface
+{
+public:
+    SpecificInterface() = default;
+    virtual ~SpecificInterface() = default;
+
+    virtual void SpecificMethod1(){}
+    virtual void SpecificMethod2(){}
+    virtual void SpecificMethod3(){}
+};
+
+class Base
+{
+    int a;
+public:
+    virtual void DoSomething() = 0;
+    virtual SpecificInterface* GetSpecificInterface(){ return nullptr; }
+};
+
+class Derived1 : public Base, public SpecificInterface
+{
+public:
+    void DoSomething() override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    SpecificInterface* GetSpecificInterface() override
+    {
+        return this;
+    }
+    void SpecificMethod1() override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    void SpecificMethod2() override
+    {
+        std::cout << __func__ << std::endl;
+    }
+    void SpecificMethod3() override
+    {
+        std::cout << __func__ << std::endl;
+    }
+};
+
+class Derived2 : public Base
+{
+public:
+    void DoSomething() override
+    {
+        std::cout << __func__ << std::endl;
+    }
+};
+
+class Foo
+{
+protected:
+    virtual void DoSomething1()
+    {
+        std::cout << "[Foo]" << __func__ << std::endl;
+    }
+private:
+    virtual void DoSomething2()
+    {
+        std::cout << "[Foo]" << __func__ << std::endl;
+    }
+};
+
+class Bar : public Foo
+{
+public:
+    void DoSomething2() override
+    {
+        this->DoSomething1();
+        std::cout << "[Bar]"<< __func__ << std::endl;
+    }
+};
+
+void main()
+{
+    Bar bar;
+    bar.DoSomething2();
+
+
+
+    std::cout << sizeof(Derived1) << std::endl;
+    Base* base1 = new Derived1();
+    base1->DoSomething();
+    if(auto p = base1->GetSpecificInterface()){
+        p->SpecificMethod1();
+        p->SpecificMethod2();
+        p->SpecificMethod3();
+    }
+
+    std::cout << sizeof(Derived2) << std::endl;
+    Base* base2 = new Derived2();
+    base2->DoSomething();
+    if(auto p = base2->GetSpecificInterface()){
+    }
+
+}
